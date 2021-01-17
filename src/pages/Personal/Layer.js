b7a949ec5d48dc37c89f1d60f5458c8249b1878d
傅譽的MacBook <b08901039@ntu.edu.tw>
2021-01-17 14:28:18 +0800

    finishLayer

--- src/pages/Personal/Layer.js
+++ src/pages/Personal/Layer.js
@@ -1,39 +1,49 @@
 import React from "react";
+import { Menu } from "antd";
 
-export default (name, color, selected, index, buttonStates) => {
-    const Selection = () =>{
-        if (selected) {
-            return <div className="layer-selected"/>;
-        }
-    }
+// export default (name, color, selected, index, buttonStates) => {
+//     const Selection = () =>{
+//         if (selected) {
+//             return <div className="layer-selected"/>;
+//         }
+//     }
 
-    const Selecting = (id) => {
-        let _newLayer = buttonStates.userData.layer;
-        _newLayer[id].selected = !_newLayer[id].selected;
-        const newUserData = {
-            userName: buttonStates.userData.userName,
-            layer: _newLayer,
-            somethingelse: buttonStates.userData.somethingelse
-        };
-        buttonStates.setUserData(newUserData);
-    }
+//     const Selecting = (id) => {
+//         let _newLayer = buttonStates.userData.layer;
+//         _newLayer[id].selected = !_newLayer[id].selected;
+//         const newUserData = {
+//             userName: buttonStates.userData.userName,
+//             layer: _newLayer,
+//             somethingelse: buttonStates.userData.somethingelse
+//         };
+//         buttonStates.setUserData(newUserData);
+//     }
 
-    const Deleting = (id) => {
-        const _newLayer = buttonStates.userData.layer.filter((ele,Id) => Id !== id);
-        const nUD = {
-            userName: buttonStates.userData.userName,
-            layer: _newLayer,
-            somethingelse: buttonStates.userData.somethingelse
-        };
-        buttonStates.setUserData(nUD);
-    }
+//     const Deleting = (id) => {
+//         const _newLayer = buttonStates.userData.layer.filter((ele,Id) => Id !== id);
+//         const nUD = {
+//             userName: buttonStates.userData.userName,
+//             layer: _newLayer,
+//             somethingelse: buttonStates.userData.somethingelse
+//         };
+//         buttonStates.setUserData(nUD);
+//     }
 
+//     return (
+//         <div className="layer" id={String(index)} >
+//             <div className="layer-color-box" style={{backgroundColor: color}} onClick={() => {Selecting(index)}}/>
+//             <div>{name}</div>
+//             {Selection()}
+//             <div className="layer-delete" onClick={() => {Deleting(index)}}></div>
+//         </div>
+//     );
+// }
+const {SubMenu} = Menu;
+
+export default (ele, index, buttonStates) => {
     return (
-        <div className="layer" id={String(index)} >
-            <div className="layer-color-box" style={{backgroundColor: color}} onClick={() => {Selecting(index)}}/>
-            <div>{name}</div>
-            {Selection()}
-            <div className="layer-delete" onClick={() => {Deleting(index)}}></div>
-        </div>
+        <SubMenu title={ele.name} >
+            
+        </SubMenu>
     );
 }
--- src/pages/Personal/LayerBar.js
+++ src/pages/Personal/LayerBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Layout, Menu, Button, Input, Drawer} from "antd";
+import { Menu, Button, Input, Modal, message } from "antd";
 import {
     PlusCircleOutlined,
     UnorderedListOutlined
@@ -9,103 +9,66 @@ import "react-bootstrap";
 import "./PersonalPage.css";
 import Layer from "./Layer";
 
-const {Header, Content, Footer, Sider} = Layout;
 const {SubMenu} = Menu;
 
 export default (buttonStates) => {
-    const [aL, setAL] = useState(false);
-    const nameRef = React.createRef();
-    const colorRef = React.createRef();
-    const layers = (buttonStates) => {
-        return buttonStates.userData.layer.map((ele, index) => Layer(ele.name,ele.color,ele.selected,index,buttonStates));
+    const [modalVisible, setModalVisible] = useState(false);
+    const layerRef = React.useRef();
+
+    const layers = () => {
+        //console.log(buttonStates.userData);
+        return buttonStates.userData.layer.map((ele, index) => Layer(ele,index,buttonStates));
     };
 
-    const addLayer = () => {
-        if (nameRef.current.value !== "" && colorRef.current.value !== "" && (buttonStates.userData.layer.find(ele => ele.name === nameRef.current.value) === undefined)){
+    const showAddModal = () => {
+        setModalVisible(true);
+    }
+
+    const cancelAdd = () => {
+        setModalVisible(false);
+    }
+
+    const comfirmAdd = () => {
+        if (layerRef.current.state.value === undefined) message.error("Please enter a name!");
+        else if (buttonStates.userData.layer.find(ele => ele.name === layerRef.current.state.value)) message.error("There already exists a same layer!");
+        else {
             const _layer = {
-                name: nameRef.current.value,
-                color: colorRef.current.value,
-                selected: true,
-                strokes: []  //schedule component
-            };
+                name: layerRef.current.state.value,
+                event: []
+            }
 
             const newUserData = {
                 userName: buttonStates.userData.userName,
                 layer: [...buttonStates.userData.layer, _layer],
                 somethingelse: buttonStates.userData.somethingelse
             }
-
             buttonStates.setUserData(newUserData);
-        }
-        else if (buttonStates.userData.layer.find(ele => ele.name === nameRef.current.value) !== undefined){
-            alert("Using same name!");
-            nameRef.current.value = "";
-        }
-        else if (nameRef.current.value === ""){
-            alert("Please enter a name!");
-            nameRef.current.value = "";
-        }
-        else if (colorRef.current.value === ""){
-            alert("Please choose a color!");
+            setModalVisible(false);
+            console.log(buttonStates.userData.layer);
         }
     }
+
     const add = (
         <Input placeholder="Enter a new layer name!" prefix={<UnorderedListOutlined />}/>
     );
 
     return (
-        /*<div className="layer-bar">
-            <div className="btn-group tool-bar">
-                <Dropdown>
-                    <Dropdown.Toggle>
-                        Add Layer
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <input type="text" placeholder="Enter a new layer name!" className="layer-name" ref={nameRef}/>
-                        <div className="layer-other">
-                            <div>Choose a color:<input type="color" className="layer-color-select" ref={colorRef}/></div>
-                            <button className="add-button" onClick={addLayer}><img src={add} /></button>  
-                        </div>  
-                    </Dropdown.Menu>
-                </Dropdown>
-            </div>
-            <div className="layer-list">
-                {layers(buttonStates)}
-            </div>
-        </div>*/
         <div>
-            <div className="logo"/>
-            testing
-        </div>
-        /*<Sider>
-            <Button type="primary" shape="round" icon={<PlusCircleOutlined />} onClick={setAL(true)}>
-                Add a layer!
-            </Button>
-            <Drawer 
-                title="Add a new layer!" 
-                width={250} 
-                onClose={setAL(false)}
-                visible={aL}
-                footer={
-                    <div
-                      style={{
-                        textAlign: 'left',
-                      }}
-                    >
-                      <Button onClick={setAL(false)} style={{ marginRight: 8 }}>
-                        Cancel
-                      </Button>
-                      <Button onClick={() => {alert("not finised")}} type="primary">
-                        Submit
-                      </Button>
-                    </div>
-                  }
+            <Button shape="circle" icon={<PlusCircleOutlined />} onClick={showAddModal}/>
+            <Modal 
+                title="Add a layer!"
+                centered
+                visible={modalVisible}
+                footer={[
+                    <Button onClick={cancelAdd}>Cancel</Button>,
+                    <Button type="primary" onClick={comfirmAdd}>Add</Button>
+                ]}
             >
-                
-                </Drawer>
-            <Menu>
-
+                <Input placeholder="Enter a new layer name" ref={layerRef}/>
+            </Modal>
+            <Menu theme="light" mode="vertical">
+                {layers()}
             </Menu>
-        </Sider>*/
+        </div>
     );
 }
